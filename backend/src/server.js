be8637965a0f4909dddd89df06e9d31ec4c4d8b0
faizan098 <faizy.kadvekar@gmail.com>
2026-09-18 12:44:03 +0530

    Test Render Express server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,56 +1,76 @@
-require("dns").setServers(["8.8.8.8", "1.1.1.1"]);
+// require("dns").setServers(["8.8.8.8", "1.1.1.1"]);
 
-require("dotenv").config();
-const errorMiddleware = require("./middleware/errorMiddleware");
+// require("dotenv").config();
+// const errorMiddleware = require("./middleware/errorMiddleware");
+// const express = require("express");
+// const cors = require("cors");
+// const helmet = require("helmet");
+// const cookieParser = require("cookie-parser");
+
+// const connectDB = require("./config/db");
+// const authRoutes = require("./routes/authRoutes");
+// const app = express();
+
+// // Connect MongoDB
+// connectDB();
+
+// // Middleware
+// app.use(helmet());
+
+// app.use(express.json());
+
+// app.use(cookieParser());
+
+// app.use(
+//     cors({
+//         origin: "https://classiq-frontend-qtp8.onrender.com",
+//         credentials: true
+//     })
+// );
+
+
+// // Health check
+// app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//     res.json({
+//         success: true,
+//         message: "ClassIQ API is running"
+//     });
+// });
+
+// app.get("/api/health", (req, res) => {
+//     res.json({
+//         success: true,
+//         message: "ClassIQ API is running"
+//     });
+// });
+
+// app.get("/test-classiq", (req, res) => {
+//     res.status(200).send("CLASSIQ BACKEND WORKING");
+// });
+
+// app.use(errorMiddleware);
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, "0.0.0.0", () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
+
+
+
 const express = require("express");
-const cors = require("cors");
-const helmet = require("helmet");
-const cookieParser = require("cookie-parser");
 
-const connectDB = require("./config/db");
-const authRoutes = require("./routes/authRoutes");
 const app = express();
 
-// Connect MongoDB
-connectDB();
-
-// Middleware
-app.use(helmet());
-
-app.use(express.json());
-
-app.use(cookieParser());
-
-app.use(
-    cors({
-        origin: "https://classiq-frontend-qtp8.onrender.com",
-        credentials: true
-    })
-);
-
-
-// Health check
-app.use("/api/auth", authRoutes);
-
 app.get("/", (req, res) => {
-    res.json({
-        success: true,
-        message: "ClassIQ API is running"
-    });
-});
-
-app.get("/api/health", (req, res) => {
-    res.json({
-        success: true,
-        message: "ClassIQ API is running"
-    });
+    res.send("CLASSIQ BACKEND IS WORKING");
 });
 
 app.get("/test-classiq", (req, res) => {
-    res.status(200).send("CLASSIQ BACKEND WORKING");
+    res.send("TEST ROUTE WORKING");
 });
-
-app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 5000;
 
